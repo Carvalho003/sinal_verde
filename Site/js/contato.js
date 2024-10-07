868ab890c4291966f1407e10f964edac4733bad4
RanierDalton <ranier.couto@sptech.school>
2024-10-07 10:28:04 -0300

    Adcionando a funcionalidade de enviar email à seção de contato

--- Site/js/contato.js
+++ Site/js/contato.js
@@ -1,7 +1,30 @@
-// podemos usar o mailto (nativo do HTML)
+function sendEmail() {
+    // fzr as validações para passar
+    // dps guardar isso em um .env
+    var email = "sinalver@exemplo.com";
+    var senha = "SENHA";
 
-// ou podemos usar o SMTP.js (essa opção é a melhor eu diria)
+    var emailContactado = ipt_email.value;
+    var nome = ipt_nome.value;
+    var assunto = ipt_assunto.value;
+    var mensagem = `
+        Nome Usuário: ${nome}
+        Email: ${emailContactado}
+        Assunto: ${assunto}
+        Mensagem: ${ipt_mensagem1.value}
+    `;
 
-//  Ou o emailJS
-
-// https://mailtrap.io/pt/blog/javascript-send-email/
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Port:2525,
+        Username: email,
+        Password: "E111EAE60628B47135DF0FE69BF520F831CC",
+        To: email,
+        From: email,
+        Subject:assunto,
+        Body: mensagem
+    })
+    .then(function (message) {
+        alert(message) // Mensagem de alerta no envio bem-sucedido do email
+    });
+}
