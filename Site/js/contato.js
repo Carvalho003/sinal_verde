30fdda62aa6ad86cd2faf2bebc774eba7ccc3c22
RanierDalton <ranier.couto@sptech.school>
2024-10-07 08:51:25 -0300

    Adicionando o script da funcionalidade do campo do contato

---
+++ Site/js/contato.js
@@ -0,0 +1,7 @@
+// podemos usar o mailto (nativo do HTML)
+
+// ou podemos usar o SMTP.js (essa opção é a melhor eu diria)
+
+//  Ou o emailJS
+
+// https://mailtrap.io/pt/blog/javascript-send-email/
