ba8e3916e556d25969bb71109ca9365e4dfead87
Hugo Carvalho <hugo.fcarvalho@sptech.school>
2024-10-09 15:38:20 -0300

    adicionar modal de adicionar colaborador

---
+++ App/js/adicionarModal.js
@@ -0,0 +1,11 @@
+const modalAdicionarPassageiro = modal_adicionar;
+
+function abrirModal() {
+    modalAdicionarPassageiro.showModal();
+}
+
+function closeModal(){
+    modalAdicionarPassageiro.close();
+}
+
+abrirModal();
