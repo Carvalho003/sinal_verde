41651cf876ae13b441e55be80455ff26824d28f5
felipesmithdev <felipe.barbosa@sptech.school>
2024-10-11 14:53:28 -0300

    Adicionar modal para adicionar empresa

---
+++ App/js/adicionarModalEmpresa.js
@@ -0,0 +1,10 @@
+const modalAdicionarEmpresa = modal_adicionar;
+
+function abrirModal() {
+    modalAdicionarEmpresa.showModal();
+}
+
+function closeModal(){
+    modalAdicionarEmpresa.close();
+    
+}
