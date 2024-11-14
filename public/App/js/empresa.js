b00708a143c9ca619f47ff648d3a3c1cd2b798d2
RanierDalton <ranier.couto@sptech.school>
2024-11-14 12:43:51 -0300

    Adicionar funcionalidade de listar empresas

--- public/App/js/empresa.js
+++ public/App/js/empresa.js
@@ -4,30 +4,30 @@ function carregarEmpresas(){
         if (response.ok) {
             response.json()
             .then((empresas) => {
-                let linhas = `<table>
-                                <tr class="comeco-tr">
+                let tabela = document.createElement("table");
+                console.log(tabela);
+                let linhas = `  <tr class="comeco-tr">
                                     <td>Nome</td>
                                     <td>Localidade</td>
                                     <td>Colaboradores</td>
                                     <td>Editar</td>
                                     <td>Excluir</td>
-                                </tr>
-                            </table>`
-                console.warn(empresas);
+                                </tr>`
+
                 empresas.forEach((empresa) => {
                     linhas += `
                         <tr class="usuario-tab">
                             <td>${empresa.nome}</td>
                             <td>${empresa.localidade}</td>
-                            <td>${empresas.colaboradores}</td>
+                            <td>${empresa.colaboradores}</td>
                             <td><i onclick="editarEmpresa(${empresa.id})"  class='bx bx-edit-alt'></i></td>
                             <td><i onclick="deletarEmpresa(${empresa.id})" class='bx bx-trash'></i></td>
                         </tr>
                     `;
                 });
-                console.log(linhas);
-                console.log(document.getElementById("tabela-empresa"));
-                document.getElementById("tabela-empresa").querySelector("tbody").inneHTML = linhas;
+                document.getElementById("tabela-empresa").appendChild(tabela);
+                tabela.innerHTML = linhas;
+                
             });
         } else {
             console.error('Nenhum dado encontrado ou erro na API');
